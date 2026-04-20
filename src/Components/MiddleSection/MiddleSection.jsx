935c60316e1ff40f06525d79994bbd47df43aa2c
mir-abdullah-mahammad-lab <mirabdullahmahammad@gmail.com>
2026-04-20 15:53:06 +0600

    responive

--- src/Components/MiddleSection/MiddleSection.jsx
+++ src/Components/MiddleSection/MiddleSection.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const MiddleSection = () => {
     return (
-        <div className='w-[100em] h-[25em] bg-linear-to-r from-[#4F39F6] to-[#9514FA] flex flex-col items-center justify-center space-x-7'>
+        <div className='w-full max-w-[100em] h-[25em] bg-linear-to-r from-[#4F39F6] to-[#9514FA] flex flex-col items-center justify-center space-x-7'>
             <div>
-                        <h1 className='text-4xl text-white '>Ready to Transform Your Workflow?</h1>
-                    <p className='text-base-300'>Join thousands of professionals who are already using Digitools to work smarter.<br></br> 
+                        <h1 className=' text-2xl md:text-4xl text-white text-center'>Ready to Transform Your Workflow?</h1>
+                    <p className='text-sm md:text-base text-base-300 text-center'>Join thousands of professionals who are already using Digitools to work smarter.<br></br> 
                     Start your free trial today.
                  </p>
             </div>
-            <div className='flex gap-4 text-center'> 
+            <div className='flex gap-4 text-center mt-10'> 
                 <button className="btn rounded-4xl bg-white "> <span className='bg-linear-to-r  from-[#4F39F6] to-[#9514FA] bg-clip-text'>Explore Products</span> </button>
                 <button className="btn btn-info rounded-4xl">View Pricing</button>
             </div>
--- src/ui/ThirdSection/ThirdSection.jsx
+++ src/ui/ThirdSection/ThirdSection.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 const ThirdSection = () => {
     return (
-        <div className='w-[100em] h-[8em] bg-linear-to-r  from-[#4F39F6] to-[#9514FA] mt-10'>
-            <div className='flex items-center justify-center '>
+        <div className='w-full max-w-[100em] h-[8em] bg-linear-to-r  from-[#4F39F6] to-[#9514FA] mt-10'>
+            <div className='flex flex-col item-centre md:flex md:flex-row items-center justify-center '>
                 <div className='mt-15 mr-20'>
-                    <h1 className='text-4xl text-white'>500+</h1>
+                    <h1 className=' text-xl md:text-4xl text-white'>500+</h1>
                     <p className='text-[10px] text-gray-300 text-center'>Active Users</p>
                 </div>
 
                 <div className='mt-15 mr-20'>
-                    <h1 className='text-4xl text-white'>200+</h1>
+                    <h1 className=' text-xl md:text-4xl text-white'>200+</h1>
                     <p className='text-[10px] text-gray-300 text-center'>Premium Tolls</p>
                 </div>
 
-                <div className='mt-15'>
-                    <h1 className='text-4xl text-white'>4.9</h1>
+                <div className='mt-15 sm:mr-20'>
+                    <h1 className='text-xl md:text-4xl text-white'>4.9</h1>
                     <p className='text-[10px] text-gray-300 text-center'>Rating</p>
                 </div>
             </div>
