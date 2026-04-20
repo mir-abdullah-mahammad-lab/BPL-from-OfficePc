390276c89233ff8d5dddc777181048c4c35655bc
mir-abdullah-mahammad-lab <mirabdullahmahammad@gmail.com>
2026-04-20 16:06:28 +0600

    all

--- src/ui/SectionSix/SectionSix.jsx
+++ src/ui/SectionSix/SectionSix.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SectionSix = () => {
     return (
-        <div className='bg-red-50 w-[1600px] h-[500px] aspect-[calc(1400/850)]'>
+        <div className='bg-red-50 w-full max-w-400 h-125 aspect-[calc(1400/850)]'>
             <div>
                 <h1 className='text-center text-3xl'>Simple, Transparent Pricing</h1>
                 <p className='text-center'>Start using premium digital tools in minutes, not hours.</p>
